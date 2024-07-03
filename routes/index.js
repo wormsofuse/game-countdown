fb59fe3737a60ff1732f2c15ef4637188465ccca
richard <richardmcallister551@gmail.com>
2024-07-03 14:44:32 +0100

    updating server output countdown with dynamic function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime = new Date("12/07/2024 10:00").getTime()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime();
+  const numberOfMilliseconds = parseInt(launchTime - currentTime)
+  res.render(
+    'index',
+     { title: 'Time until I cry', countDown: numberOfMilliseconds });
 });
 
 module.exports = router;
